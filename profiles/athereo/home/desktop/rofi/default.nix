13da14f0f6b454233ac3fd7a4ba488ce1129b747
Athereo <athereoandromeda@gmail.com>
2024-08-12 20:45:57 +0800

    yeah

--- profiles/athereo/home/desktop/rofi/default.nix
+++ profiles/athereo/home/desktop/rofi/default.nix
@@ -1,10 +1,25 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   home.packages = with pkgs; [
     rofi-wayland
   ];
 
   xdg.configFile = {
-    "rofi/themes/rounded-common.rasi".source = ./rounded-common.rasi;
-    "rofi/themes/rounded-blue-dark.rasi".source = ./rounded-blue-dark.rasi;
+    "rofi/themes".source = ./themes;
+    "rofi/shortcuts".source = ./shortcuts;
+
+    "rofi/colors/base16.rasi".text = with config.scheme.withHashtag; ''
+      * {
+        background:     ${base00};
+        background-alt: ${base04};
+        foreground:     ${base05};
+        selected:       ${base03};
+        active:         ${base02};
+        urgent:         ${red};
+      }
+    '';
   };
 }
---
+++ profiles/athereo/home/desktop/rofi/shortcuts/launch-launcher.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rofi -show drun -show-icons -theme $HOME/.config/rofi/themes/rounded-blue-dark.rasi
