bf2750d44e39e642e716ee54272bfbe9f029d6f3
Athereo <athereoandromeda@gmail.com>
2024-08-25 23:49:04 +0800

    style/swaync-scss: Convert CSS styles to SCSS

--- profiles/athereo/home/desktop/swaync/default.nix
+++ profiles/athereo/home/desktop/swaync/default.nix
@@ -1,12 +1,42 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  build-scss = path:
+    pkgs.stdenv.mkDerivation {
+      name = "sass-builder";
+      src = path;
+
+      nativeBuildInputs = with pkgs; [
+        dart-sass
+        # python312
+      ];
+
+      dontUnpack = true;
+      dontPatch = true;
+      dontConfigure = true;
+      dontInstall = true;
+
+      # mkdir build
+
+      # cd $src
+      # python filter.py
+      # cd ..
+      # echo '${base16-text}' > build/_base16.scss;
+
+      # ls build
+      # sass build/:$out/dist
+      buildPhase = ''
+        mkdir -p $out
+
+        echo "Building SCSS..."
+        sass $src:$out/dist
+      '';
+    };
+in {
   home.packages = with pkgs; [
     swaynotificationcenter
   ];
 
   xdg.configFile = {
-    "swaync" = {
-      source = ./config;
-      recursive = true;
-    };
+    "swaync/config.json".source = ./config/config.json;
+    "swaync/dist".source = "${build-scss ./config/styles}/dist";
   };
 }
